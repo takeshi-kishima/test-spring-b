1540fb27ff9b81ab62b357eca53c8c1f4e4a7243
takeshi-kishima <yahoo.fukuoka@gmail.com>
2021-04-19 22:53:45 +0900

    Same Origin Policy（同一オリジンポリシー） Cross-Origin Resource Sharing (CORS)、許容する Origin を設定した

--- src/main/java/com/gmail/fukuoka/yahoo/testspringb/GreetingController.java
+++ src/main/java/com/gmail/fukuoka/yahoo/testspringb/GreetingController.java
@@ -2,6 +2,7 @@ package com.gmail.fukuoka.yahoo.testspringb;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
+    @CrossOrigin(origins = {"http://localhost:3000"})
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
